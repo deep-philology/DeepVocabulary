74d2d8dbbb1763369c3030c501c5cbedf655b8d3
James Tauber <jtauber@jtauber.com>
2017-12-05 18:12:43 -0500

    pinax starter project

---
+++ static/src/js/index.js
@@ -0,0 +1,33 @@
+/* global window document */
+window.jQuery = window.$ = require('jquery');
+
+const $ = window.$;
+
+require('bootstrap/dist/js/bootstrap.bundle');
+
+import ajaxSendMethod from './ajax';
+import handleMessageDismiss from './messages';
+import loadStripeElements from './pinax-stripe';
+import hookupCustomFileWidget from './pinax-documents';
+
+$(() => {
+    $(document).ajaxSend(ajaxSendMethod);
+
+    // Topbar active tab support
+    $('.topbar li').removeClass('active');
+
+    const classList = $('body').attr('class').split(/\s+/);
+    $.each(classList, (index, item) => {
+        const selector = `ul.nav li#tab_${item}`;
+        $(selector).addClass('active');
+    });
+
+    $('#account_logout, .account_logout').click(e => {
+        e.preventDefault();
+        $('#accountLogOutForm').submit();
+    });
+
+    handleMessageDismiss();
+    loadStripeElements();
+    hookupCustomFileWidget();
+});
