1f664013940c87545bcbccecabd86d19f1021b7d
James Tauber <jtauber@jtauber.com>
2017-12-06 17:21:59 -0500

    view that takes lemma text in URL and redirects to detail page
    fixes #9

--- deep_vocabulary/urls.py
+++ deep_vocabulary/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 
 from django.contrib import admin
 
-from .views import LemmaDetail
+from .views import LemmaDetail, lemma_by_text
 
 
 urlpatterns = [
@@ -14,6 +14,7 @@ urlpatterns = [
     url(r"^account/", include("account.urls")),
 
     url(r"^lemma/(?P<pk>\d+)/$", LemmaDetail.as_view(), name="lemma_detail"),
+    url(r"^lemma/text/(?P<text>[^/]+)/$", lemma_by_text, name="lemma_by_text"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- deep_vocabulary/views.py
+++ deep_vocabulary/views.py
@@ -1,7 +1,14 @@
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import DetailView
+
 from .models import Lemma
 
 
 class LemmaDetail(DetailView):
 
     model = Lemma
+
+
+def lemma_by_text(request, text):
+    lemma = get_object_or_404(Lemma, text=text)
+    return redirect("lemma_detail", pk=lemma.pk)
